8c3b6d79e1149211419ee3c19d328188f569e7b4
George Gehring <george.github@ggehring.com>
2025-02-28 22:16:13 -0500

    First React Program
    My first hands on experience with React and Java Script

---
+++ my-react-app/src/footer.jsx
@@ -0,0 +1,19 @@
+
+function Footer(){
+
+
+
+return(
+
+<footer>
+
+<p>&copy;{new Date().getFullYear()}Robbys Website</p>
+
+</footer>
+
+
+);
+
+}
+
+export default Footer
---
+++ my-react-app/src/header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+    return(<header>
+        <h1>My Website</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+
+        </nav>
+    <hr></hr>
+
+    </header>);
+}
+
+export default Header
